6a3d620e5e45cc48718906c8d8ecb11377b7ddb5
Astha4Study <iyandabes1@gmail.com>
2025-04-02 19:53:19 +0700

    add copy from rollup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,11 +14,14 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    copy({
+      targets: [{ src: "CNAME", dest: "dist" }]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',
+  base: '/Astha-portfolio/',
 }));
